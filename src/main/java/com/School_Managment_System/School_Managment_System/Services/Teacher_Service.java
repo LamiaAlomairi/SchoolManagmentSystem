4cd38fdba33b09797104e24d42975dd12557028b
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:34:53 +0400

    Add code to Teacher_Service class

--- src/main/java/com/School_Managment_System/School_Managment_System/Services/Teacher_Service.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Services/Teacher_Service.java
@@ -1,5 +1,22 @@
 package com.School_Managment_System.School_Managment_System.Services;
 
-public class Teacher_Service {
+import com.School_Managment_System.School_Managment_System.Models.Teacher;
+import com.School_Managment_System.School_Managment_System.Repositories.Teacher_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+@Service
+public class Teacher_Service {
+    @Autowired
+    Teacher_Repository teacher_repository;
+    public List<Teacher> getAllTeachers() {
+
+        return teacher_repository.findAll();
+    }
+
+    public Teacher getTeacherById(Long id) {
+
+        return teacher_repository.findById(id).get();
+    }
 }
