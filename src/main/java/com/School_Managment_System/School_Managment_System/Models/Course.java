16789c8884bf1f4d2f15196555eaafb3bf291a7d
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 11:33:07 +0400

    Add relationships to Course class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Course.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Course.java
@@ -1,10 +1,12 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -18,4 +20,12 @@ public class Course {
     String course_name;
     Float grade;
     String course_class;
+
+    @ManyToMany(mappedBy = "courses")
+    @JsonIgnore
+    private List<Student> students;
+
+    @OneToOne
+    @JoinColumn(name = "teacher_id", referencedColumnName = "teacher_id")
+    Teacher teacher;
 }
