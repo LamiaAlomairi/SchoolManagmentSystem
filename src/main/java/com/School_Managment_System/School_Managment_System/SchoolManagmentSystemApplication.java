19a0a66bd97d6fd0a8cadec416550617e72f2f52
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 09:58:16 +0400

    Create School_Managment_System Project

---
+++ src/main/java/com/School_Managment_System/School_Managment_System/SchoolManagmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.School_Managment_System.School_Managment_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/School_Managment_System/School_Managment_System/SchoolManagmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.School_Managment_System.School_Managment_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolManagmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
