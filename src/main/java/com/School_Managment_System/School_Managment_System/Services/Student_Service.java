9d38104eee287ffd3897856c8e999b263f5d9de4
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:37:16 +0400

    Add code to Student_Service class

--- src/main/java/com/School_Managment_System/School_Managment_System/Services/Student_Service.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Services/Student_Service.java
@@ -1,5 +1,25 @@
 package com.School_Managment_System.School_Managment_System.Services;
 
-public class Student_Service {
+import com.School_Managment_System.School_Managment_System.Models.Student;
+import com.School_Managment_System.School_Managment_System.Models.Teacher;
+import com.School_Managment_System.School_Managment_System.Repositories.Student_Repository;
+import com.School_Managment_System.School_Managment_System.Repositories.Teacher_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class Student_Service {
+    @Autowired
+    Student_Repository student_repository;
+    public List<Student> getAllStudents() {
+
+        return student_repository.findAll();
+    }
+
+    public Student getStudentById(Long id) {
+
+        return student_repository.findById(id).get();
+    }
 }
