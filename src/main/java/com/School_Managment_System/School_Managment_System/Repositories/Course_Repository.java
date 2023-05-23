243c21e97da726448a38603869174e52bb077e1c
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:23:09 +0400

    Add extends to Course_Repository

--- src/main/java/com/School_Managment_System/School_Managment_System/Repositories/Course_Repository.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Repositories/Course_Repository.java
@@ -1,4 +1,7 @@
 package com.School_Managment_System.School_Managment_System.Repositories;
 
-public interface Course_Repository {
+import com.School_Managment_System.School_Managment_System.Models.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Course_Repository extends JpaRepository<Course, Long> {
 }
