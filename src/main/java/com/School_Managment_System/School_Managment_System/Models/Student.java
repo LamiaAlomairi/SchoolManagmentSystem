97c6b7a29147adcf641d4cc8bca0550bcf8ef610
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:16:51 +0400

    Add attributes to Student class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Student.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Student.java
@@ -1,4 +1,22 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long student_id;
+    String name;
+    String gender;
+    Integer age;
+    String parents_number;
 }
