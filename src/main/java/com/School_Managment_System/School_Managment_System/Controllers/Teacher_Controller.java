0831514c151e47652a99179073dcdc892593c818
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:51:34 +0400

    Add get code to Teacher_Controller class

--- src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Teacher_Controller.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Teacher_Controller.java
@@ -1,5 +1,25 @@
 package com.School_Managment_System.School_Managment_System.Controllers;
 
-public class Teacher_Controller {
+import com.School_Managment_System.School_Managment_System.Models.Teacher;
+import com.School_Managment_System.School_Managment_System.Services.Teacher_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "teacher")
+public class Teacher_Controller {
+    @Autowired
+    Teacher_Service teacher_service;
+    @GetMapping(value = "getAll")
+    public List<Teacher> getAllTeachers() {
+
+        return teacher_service.getAllTeachers();
+    }
+    @GetMapping(value = "getById")
+    public Teacher getTeacherById(@RequestParam Long id) {
+
+        return teacher_service.getTeacherById(id);
+    }
 }
