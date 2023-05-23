8ddcd57560d781c77481ae5c050ec4e0cadf252b
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 12:57:23 +0400

    Add attributes to School_Class class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/School_Class.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/School_Class.java
@@ -1,4 +1,21 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "school_class")
 public class School_Class {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String name;
+    Float size;
+    String class_code;
 }
