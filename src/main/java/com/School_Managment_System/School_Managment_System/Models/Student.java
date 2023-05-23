0ad326b986be2a38b2e4c7ad7005bf8dad32a49b
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 11:40:52 +0400

    Add relationship to Student class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Student.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Student.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Getter
 @Setter
@@ -19,4 +20,8 @@ public class Student {
     String gender;
     Integer age;
     String parents_number;
+
+    @ManyToMany
+    @JoinTable(name = "student_course", joinColumns = @JoinColumn(name = "student_id"), inverseJoinColumns = @JoinColumn(name = "course_id"))
+    private List<Course> courses;
 }
