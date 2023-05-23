b9699ca01d2d2d841fef47902077453b9395f1eb
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:55:19 +0400

    Add get code to Student_Controller class

--- src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Student_Controller.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Student_Controller.java
@@ -1,5 +1,30 @@
 package com.School_Managment_System.School_Managment_System.Controllers;
 
-public class Student_Controller {
+import com.School_Managment_System.School_Managment_System.Models.Student;
+import com.School_Managment_System.School_Managment_System.Models.Teacher;
+import com.School_Managment_System.School_Managment_System.Services.Student_Service;
+import com.School_Managment_System.School_Managment_System.Services.Teacher_Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "student")
+public class Student_Controller {
+    @Autowired
+    Student_Service student_service;
+    @GetMapping(value = "getAll")
+    public List<Student> getAllStudents() {
+
+        return student_service.getAllStudents();
+    }
+    @GetMapping(value = "getById")
+    public Student getStudentById(@RequestParam Long id) {
+
+        return student_service.getStudentById(id);
+    }
 }
