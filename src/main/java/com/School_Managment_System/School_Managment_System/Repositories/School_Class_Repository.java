b887ce2393c49fa90a13c972505899686603e79a
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 13:03:03 +0400

    Create School_Class_Repository interface with extends

---
+++ src/main/java/com/School_Managment_System/School_Managment_System/Repositories/School_Class_Repository.java
@@ -0,0 +1,7 @@
+package com.School_Managment_System.School_Managment_System.Repositories;
+
+import com.School_Managment_System.School_Managment_System.Models.School_Class;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface School_Class_Repository extends JpaRepository<School_Class, Long> {
+}
