397cb124cf658d1a10977066614a96615a97c0da
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:40:03 +0400

    Add code to Course_Service class

--- src/main/java/com/School_Managment_System/School_Managment_System/Services/Course_Service.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Services/Course_Service.java
@@ -1,5 +1,25 @@
 package com.School_Managment_System.School_Managment_System.Services;
 
-public class Course_Service {
+import com.School_Managment_System.School_Managment_System.Models.Course;
+import com.School_Managment_System.School_Managment_System.Models.Student;
+import com.School_Managment_System.School_Managment_System.Repositories.Course_Repository;
+import com.School_Managment_System.School_Managment_System.Repositories.Student_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class Course_Service {
+    @Autowired
+    Course_Repository course_repository;
+    public List<Course> getAllCourses() {
+
+        return course_repository.findAll();
+    }
+
+    public Course getCourseById(Long id) {
+
+        return course_repository.findById(id).get();
+    }
 }
