8a82d25432c434f0a7af1658bcf8df0a997cbdad
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-23 10:13:07 +0400

    Add attributes to Teacher class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Teacher.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Teacher.java
@@ -1,4 +1,23 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "teacher")
 public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long teacher_id;
+    String name;
+    String gender;
+    Integer age;
+    String phone_number;
+    String major;
 }
