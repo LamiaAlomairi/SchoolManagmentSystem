0c2b4f7176d8744d4978799252d13409f4d3ce27
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 10:39:46 +0400

    remove unused imports

--- src/main/java/com/School_Managment_System/School_Managment_System/Services/Student_Service.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Services/Student_Service.java
@@ -1,10 +1,7 @@
 package com.School_Managment_System.School_Managment_System.Services;
 
-import com.School_Managment_System.School_Managment_System.Models.Course;
 import com.School_Managment_System.School_Managment_System.Models.Student;
-import com.School_Managment_System.School_Managment_System.Models.Teacher;
 import com.School_Managment_System.School_Managment_System.Repositories.Student_Repository;
-import com.School_Managment_System.School_Managment_System.Repositories.Teacher_Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
