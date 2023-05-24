ba149ec31fca2ec079b27f4e68506dfbb7ccbbf6
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 20:57:50 +0400

    Create Student_Course_Controller class

---
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Student_Course_Controller.java
@@ -0,0 +1,9 @@
+package com.School_Managment_System.School_Managment_System.Controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "student_course")
+public class Student_Course_Controller {
+
+}
