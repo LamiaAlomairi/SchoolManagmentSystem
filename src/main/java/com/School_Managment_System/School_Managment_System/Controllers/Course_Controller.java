789586aef80e783bb02617de2a65aa5435758426
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 10:56:54 +0400

    Add delete method to Course_Conroller class

--- src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Course_Controller.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Course_Controller.java
@@ -3,6 +3,7 @@ package com.School_Managment_System.School_Managment_System.Controllers;
 import com.School_Managment_System.School_Managment_System.Models.Course;
 import com.School_Managment_System.School_Managment_System.Services.Course_Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +28,10 @@ public class Course_Controller {
     public String addCourse(@RequestBody Course course){
         course_service.addCourse(course);
         return "Course added";
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteCourse(@PathVariable long id){
+
     }
 }
