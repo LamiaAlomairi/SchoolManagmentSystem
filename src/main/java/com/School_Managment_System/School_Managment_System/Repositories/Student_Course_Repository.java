593b599c9db54a8002ce28c192ac9b21b72fcf74
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 19:59:20 +0400

    Create Student_Course_Repository interface with extends

---
+++ src/main/java/com/School_Managment_System/School_Managment_System/Repositories/Student_Course_Repository.java
@@ -0,0 +1,7 @@
+package com.School_Managment_System.School_Managment_System.Repositories;
+
+import com.School_Managment_System.School_Managment_System.Models.Student_Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Student_Course_Repository extends JpaRepository<Student_Course, Long> {
+}
