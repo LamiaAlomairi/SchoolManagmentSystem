f7bfd499dc115aed7c4b06e411a232fd6349ab93
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 21:01:46 +0400

    Create addStudentCourse method with code

--- src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Student_Course_Controller.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/Student_Course_Controller.java
@@ -1,9 +1,19 @@
 package com.School_Managment_System.School_Managment_System.Controllers;
 
+import com.School_Managment_System.School_Managment_System.Models.Student_Course;
+import com.School_Managment_System.School_Managment_System.Services.Student_Course_Service;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "student_course")
 public class Student_Course_Controller {
+    @Autowired
+    Student_Course_Service studentCourseService;
 
+    @PostMapping(value = "add")
+    public String addStudentCourse(@RequestBody Student_Course studentCourse){
+        studentCourseService.addStudentCourse(studentCourse);
+        return "Information added";
+    }
 }
