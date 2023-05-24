16f95271008ca5b5d0061126c01d146e966f762f
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 16:30:00 +0400

    Add attribute to @Id

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Student_Course.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Student_Course.java
@@ -1,4 +1,11 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 public class Student_Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
 }
