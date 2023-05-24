176000c0e754f986f2712e215917769f34356068
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 20:55:59 +0400

    Add method name add student course in Student course service class

--- src/main/java/com/School_Managment_System/School_Managment_System/Services/Student_Course_Service.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Services/Student_Course_Service.java
@@ -1,8 +1,17 @@
 package com.School_Managment_System.School_Managment_System.Services;
 
+import com.School_Managment_System.School_Managment_System.Models.Student_Course;
+import com.School_Managment_System.School_Managment_System.Repositories.Student_Course_Repository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class Student_Course_Service {
+    @Autowired
+    Student_Course_Repository studentCourseRepository;
 
+    public void addStudentCourse(Student_Course studentCourse){
+
+        studentCourseRepository.save(studentCourse);
+    }
 }
