db9b2bccaaab5b1832ba0a5ccb5c659ac125928d
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 11:02:16 +0400

    change relationship in teacher class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Teacher.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Teacher.java
@@ -1,9 +1,10 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-
+import java.util.*;
 import javax.persistence.*;
 
 @Getter
@@ -23,7 +24,7 @@ public class Teacher {
     String phone_number;
     String major;
 
-    @OneToOne
-    @JoinColumn(name = "course_id", referencedColumnName = "course_id")
-    Course course;
+    @OneToMany
+    @JsonIgnore
+    private List<Course> courses;
 }
