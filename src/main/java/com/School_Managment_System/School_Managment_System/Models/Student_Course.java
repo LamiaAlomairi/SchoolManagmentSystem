ba1f7df6c5f5bff8be2a148f1ed62268873a5cba
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-24 16:45:44 +0400

    Add relationship to student_course class

--- src/main/java/com/School_Managment_System/School_Managment_System/Models/Student_Course.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Models/Student_Course.java
@@ -1,11 +1,26 @@
 package com.School_Managment_System.School_Managment_System.Models;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+@Table(name = "student_course")
 public class Student_Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", referencedColumnName = "student_id")
+    Student student;
+
+    @ManyToOne
+    @JoinColumn(name = "course_id", referencedColumnName = "course_id")
+    Course course;
 }
