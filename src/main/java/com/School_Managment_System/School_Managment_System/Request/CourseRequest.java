b99ca3d43faf6226bc6a524126fe01bed5a75757
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-25 11:47:07 +0400

    Create CourseRequest class

---
+++ src/main/java/com/School_Managment_System/School_Managment_System/Request/CourseRequest.java
@@ -0,0 +1,15 @@
+package com.School_Managment_System.School_Managment_System.Request;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Data
+@Entity
+public class CourseRequest {
+    
+}
