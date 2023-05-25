0366bfc74aca9c8d59fb73d53cf56f9cb90b1b78
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-25 10:59:12 +0400

    Modification of CourseController class

--- src/main/java/com/School_Managment_System/School_Managment_System/Controllers/CourseController.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/CourseController.java
@@ -1,7 +1,7 @@
 package com.School_Managment_System.School_Managment_System.Controllers;
 
 import com.School_Managment_System.School_Managment_System.Models.Course;
-import com.School_Managment_System.School_Managment_System.Services.Course_Service;
+import com.School_Managment_System.School_Managment_System.Services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,35 +10,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "course")
-public class Course_Controller {
+public class CourseController {
     @Autowired
-    Course_Service course_service;
+    CourseService courseService;
     @GetMapping(value = "getAll")
     public List<Course> getAllCourses() {
 
-        return course_service.getAllCourses();
+        return courseService.getAllCourses();
     }
     @GetMapping(value = "getById")
     public Course getCourseById(@RequestParam Long id) {
 
-        return course_service.getCourseById(id);
+        return courseService.getCourseById(id);
     }
 
     @PostMapping(value = "add")
     public String addCourse(@RequestBody Course course){
-        course_service.addCourse(course);
+        courseService.addCourse(course);
         return "Course added";
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteCourse(@PathVariable long id){
-        course_service.deleteCourse(id);
+        courseService.deleteCourse(id);
         return ResponseEntity.ok("Course deleted successfully.");
     }
 
     @PutMapping("{id}")
     public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course updateData){
-        Course course = course_service.updateCourse(id, updateData);
+        Course course = courseService.updateCourse(id, updateData);
         if (course != null) {
             return ResponseEntity.ok(course);
         } else {
