4d98410250d1c0b2aa759d429d4675a89d6c3420
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-25 10:47:17 +0400

    Modification of CourseService class

--- src/main/java/com/School_Managment_System/School_Managment_System/Services/CourseService.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Services/CourseService.java
@@ -1,37 +1,37 @@
 package com.School_Managment_System.School_Managment_System.Services;
 
 import com.School_Managment_System.School_Managment_System.Models.Course;
-import com.School_Managment_System.School_Managment_System.Repositories.Course_Repository;
+import com.School_Managment_System.School_Managment_System.Repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class Course_Service {
+public class CourseService {
     @Autowired
-    Course_Repository course_repository;
+    CourseRepository courseRepository;
     public List<Course> getAllCourses() {
 
-        return course_repository.findAll();
+        return courseRepository.findAll();
     }
 
     public Course getCourseById(Long id) {
 
-        return course_repository.findById(id).get();
+        return courseRepository.findById(id).get();
     }
 
     public void addCourse(Course course){
 
-        course_repository.save(course);
+        courseRepository.save(course);
     }
 
     public void deleteCourse(Long id) {
-        course_repository.deleteById(id);
+        courseRepository.deleteById(id);
     }
 
     public Course updateCourse(Long id, Course updateData){
-        Course course = course_repository.findById(id).orElse(null);
+        Course course = courseRepository.findById(id).orElse(null);
         if (course != null) {
             course.setCourse_name(updateData.getCourse_name());
             course.setCourse_code(updateData.getCourse_code());
@@ -39,7 +39,7 @@ public class Course_Service {
             course.setStudent_courses(updateData.getStudent_courses());
             course.setClass_room(updateData.getClass_room());
 
-            return course_repository.save(course);
+            return courseRepository.save(course);
         }
         return null;
     }
