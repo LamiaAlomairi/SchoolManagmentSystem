85ecfa774f731b80af522c08c07c3ce77df22402
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-27 13:22:55 +0400

    Add code request to StudentCourseRequest class

--- src/main/java/com/School_Managment_System/School_Managment_System/Request/StudentCourseRequest.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Request/StudentCourseRequest.java
@@ -1,15 +1,38 @@
-//package com.School_Managment_System.School_Managment_System.Request;
-//
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.*;
-//
-//@Getter
-//@Setter
-//@Data
-//@Entity
-//public class StudentCourseRequest {
-//
-//}
+package com.School_Managment_System.School_Managment_System.Request;
+
+import com.School_Managment_System.School_Managment_System.Models.StudentCourse;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+public class StudentCourseRequest {
+    Double grade;
+
+    public static StudentCourse convert(StudentCourseRequest request) {
+        Date nowDate = new Date();
+        StudentCourse studentCourse = new StudentCourse();
+
+        studentCourse.setGrade(request.getGrade());
+        studentCourse.setIsActive(true);
+        studentCourse.setCreatedDate(nowDate);
+        studentCourse.setUpdatedDate(nowDate);
+        return studentCourse;
+    }
+
+    public static List<StudentCourse> convert(List<StudentCourseRequest> requestList) {
+        List<StudentCourse> studentCourses = new ArrayList<>();
+        if (!requestList.isEmpty()) {
+            for (StudentCourseRequest studentCourseRequest : requestList) {
+                studentCourses.add(convert(studentCourseRequest));
+            }
+        }
+        return studentCourses;
+    }
+}
