951604610a9eede8d3e781a421c67e4d55b07403
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-27 13:05:11 +0400

    Add attributes and code to convert request to response in StudentResponse class

--- src/main/java/com/School_Managment_System/School_Managment_System/Response/StudentResponse.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Response/StudentResponse.java
@@ -1,15 +1,42 @@
-//package com.School_Managment_System.School_Managment_System.Response;
-//
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.Entity;
-//
-//@Getter
-//@Setter
-//@Data
-//@Entity
-//public class StudentResponse {
-//
-//}
+package com.School_Managment_System.School_Managment_System.Response;
+
+import com.School_Managment_System.School_Managment_System.Models.Student;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+@Builder
+public class StudentResponse {
+    Long id;
+    String name;
+    Integer age;
+    String email;
+    String gender;
+
+    public static StudentResponse convertRequestToResponse(Student studentRequest) {
+        return StudentResponse.builder()
+                .id(studentRequest.getId())
+                .name(studentRequest.getName())
+                .age(studentRequest.getAge())
+                .email(studentRequest.getEmail())
+                .gender(studentRequest.getGender())
+                .build();
+    }
+
+    public static List<StudentResponse> convertRequestListToResponseList(List<Student> studentRequest) {
+        List<StudentResponse> studentResponseList = new ArrayList<>();
+        if (!studentRequest.isEmpty()) {
+            for (Student studentsRequest : studentRequest) {
+                studentResponseList.add(convertRequestToResponse(studentsRequest));
+            }
+        }
+        return studentResponseList;
+    }
+}
