898960eb4544e6644673c7b9c23fc07988723ba3
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-27 12:15:14 +0400

    Add code to create new course in CourseRequest class

--- src/main/java/com/School_Managment_System/School_Managment_System/Request/CourseRequest.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Request/CourseRequest.java
@@ -1,10 +1,13 @@
 package com.School_Managment_System.School_Managment_System.Request;
 
+import com.School_Managment_System.School_Managment_System.Models.Course;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
@@ -13,4 +16,27 @@ public class CourseRequest {
     String name;
     String description;
     String courseCode;
+
+    public static Course convert(CourseRequest request) {
+        Date nowDate = new Date();
+        Course course = new Course();
+
+        course.setName(request.getName());
+        course.setDescription(request.getDescription());
+        course.setCourseCode(request.getCourseCode());
+        course.setIsActive(true);
+        course.setCreatedDate(nowDate);
+        course.setUpdatedDate(nowDate);
+        return course;
+    }
+
+    public static List<Course> convert(List<CourseRequest> requestList) {
+        List<Course> courses = new ArrayList<>();
+        if (!requestList.isEmpty()) {
+            for (CourseRequest courseRequest : requestList) {
+                courses.add(convert(courseRequest));
+            }
+        }
+        return courses;
+    }
 }
