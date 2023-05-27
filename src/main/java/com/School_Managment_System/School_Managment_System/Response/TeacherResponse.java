43f0c8eeab8364f2a421acb8b6fbc7646becb8ef
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-27 14:09:01 +0400

    Add code in TeacherResponse class

--- src/main/java/com/School_Managment_System/School_Managment_System/Response/TeacherResponse.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Response/TeacherResponse.java
@@ -1,15 +1,42 @@
-//package com.School_Managment_System.School_Managment_System.Response;
-//
-//import lombok.Data;
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.Entity;
-//
-//@Getter
-//@Setter
-//@Data
-//@Entity
-//public class TeacherResponse {
-//
-//}
+package com.School_Managment_System.School_Managment_System.Response;
+
+import com.School_Managment_System.School_Managment_System.Models.Teacher;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Data
+@Builder
+public class TeacherResponse {
+    Long id;
+    String name;
+    String phoneNumber;
+    String specialization;
+    Integer age;
+
+    public static TeacherResponse convertRequestToResponse(Teacher teacherRequest) {
+        return TeacherResponse.builder()
+                .id(teacherRequest.getId())
+                .name(teacherRequest.getName())
+                .phoneNumber(teacherRequest.getPhoneNumber())
+                .specialization(teacherRequest.getSpecialization())
+                .age(teacherRequest.getAge())
+                .build();
+    }
+
+    public static List<TeacherResponse> convertRequestListToResponseList(List<Teacher> teacherRequest) {
+        List<TeacherResponse> teacherResponseList = new ArrayList<>();
+        if (!teacherRequest.isEmpty()) {
+            for (Teacher Request : teacherRequest) {
+                teacherResponseList.add(convertRequestToResponse(Request));
+            }
+        }
+        return teacherResponseList;
+    }
+}
