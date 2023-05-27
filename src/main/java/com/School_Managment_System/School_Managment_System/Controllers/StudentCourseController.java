dd82664fa6a18e51b0e8e40e9aa133a4e80d637f
LamiaAlomairi <lama5alomairi@gmail.com>
2023-05-27 13:39:20 +0400

    Update addStudentCourse method code in StudentCourseController class

--- src/main/java/com/School_Managment_System/School_Managment_System/Controllers/StudentCourseController.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/Controllers/StudentCourseController.java
@@ -1,6 +1,7 @@
 package com.School_Managment_System.School_Managment_System.Controllers;
 
 import com.School_Managment_System.School_Managment_System.Models.StudentCourse;
+import com.School_Managment_System.School_Managment_System.Request.StudentCourseRequest;
 import com.School_Managment_System.School_Managment_System.Services.StudentCourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -14,9 +15,8 @@ public class StudentCourseController {
     StudentCourseService studentCourseService;
 
     @PostMapping(value = "add")
-    public String addStudentCourse(@RequestBody StudentCourse studentCourse){
-        studentCourseService.addStudentCourse(studentCourse);
-        return "Information added";
+    public void addStudentCourse(@RequestBody StudentCourseRequest studentCourseRequest){
+        studentCourseService.addStudentCourse(studentCourseRequest);
     }
 
     @GetMapping(value = "getAll")
