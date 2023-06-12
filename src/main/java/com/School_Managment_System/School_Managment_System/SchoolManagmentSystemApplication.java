bf0bc33dc7c066a6a43fbb7c2f960fdb2f054c45
LamiaAlomairi <lama5alomairi@gmail.com>
2023-06-12 18:55:30 +0400

    Do some change in security code and check if running

--- src/main/java/com/School_Managment_System/School_Managment_System/SchoolManagmentSystemApplication.java
+++ src/main/java/com/School_Managment_System/School_Managment_System/SchoolManagmentSystemApplication.java
@@ -6,10 +6,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import javax.persistence.Convert;
+import javax.persistence.Converter;
+
 @SpringBootApplication
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
-@ComponentScan(basePackages = {"com.School_Managment_System.School_Managment_System", "com.School_Managment_System.School_Managment_System.Configuration"})
+//@ComponentScan(basePackages = {"com.School_Managment_System.School_Managment_System", "com.School_Managment_System.School_Managment_System.Configuration"})
 public class SchoolManagmentSystemApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SchoolManagmentSystemApplication.class, args);
